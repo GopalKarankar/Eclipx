9ea115dee9af70f270a8f7b864480bd2a3e8541f
Gopal Karankar <karankargopal0gmail.com>
2025-11-19 19:17:53 +0530

    done

--- frontend/src/components/Preloader.jsx
+++ frontend/src/components/Preloader.jsx
@@ -10,6 +10,13 @@ const spin = keyframes`
 const LoaderWrapper = styled.div`
   width: 100%;
   height: 100vh;
+  
+  /* For centering */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
   display: flex;
   align-items: center;
   justify-content: center;
