4f7afd9013cf03bf3841f994dec3a5c541053187
Gopal Karankar <karankargopal0gmail.com>
2025-11-25 19:50:24 +0530

    done

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -9,7 +9,7 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_GOOGLE_API_KEY}`,
+  apiKey: "AIzaSyAmx6n4IDQpSxWBcu93sDbqytIYYKnlmhY",
   authDomain: "video-1f884.firebaseapp.com",
   projectId: "video-1f884",
   storageBucket: "video-1f884.firebasestorage.app",
