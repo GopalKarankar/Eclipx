76d80113e77e09f573e26998e10aea9cf09b2eb8
Gopal Karankar <karankargopal0gmail.com>
2026-01-30 18:35:43 +0530

    done

--- backend/verifyToken.js
+++ backend/verifyToken.js
@@ -9,7 +9,7 @@ export const verifyToken =  (req, res, next) =>{
         
         const token = req.cookies.access_token;
 
-        console.log(token);
+        console.log("Token : ",token);
 
         if (!token) {
              return next(createError(401,"You are not authenticated"));
